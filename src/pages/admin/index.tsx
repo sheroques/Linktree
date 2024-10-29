196fa9eeb01cd656741240972eebbf051c12b1c4
sheroques <luizalexandre99@hotmail.com.br>
2024-10-29 15:11:56 -0300

    Atualizacao-do-forms

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,9 +1,40 @@
-import {Header} from '../../components/Header'
 
+import {Header} from '../../components/Header'
+import { Input } from '../../components/input'
+import { useState } from 'react'
 export function Admin(){
+    const [nameInput, setNameInput] = useState("")
+    const [urlInput, setUrlInput] = useState("")
+    const [textColorInput, setTextColorInput] = useState("#f1f1f1")
+    const [backgroundColorInput, setBackgroundColorInput] = useState('#121212')
+
     return(
         <div className="flex items-center flex-col min-h-screen pb-7 px-2">
             <Header/>
+            <form className='flex flex-col mt-8 mb-3 w-full max-w-xl'>
+                <label className='text-white font-medium mt-2 mb-2 '>Nome do Link</label>
+                <Input placeholder ="Digite o nome do Link" value={nameInput} onChange={ (e)=>setNameInput(e.target.value)} />
+                
+                <label className='text-white font-medium mt-2 mb-2 '>Url do link</label>
+                <Input placeholder ="Digite a url" value={urlInput} onChange={ (e)=>setUrlInput(e.target.value)} />
+
+                <section className='flex my-4 gap-5'>
+                    <div>
+                        <label className='text-white font-medium mt-2 mb-3'>Cordo link</label>
+                        <input type = "color" value = {textColorInput} onChange={(e) => setTextColorInput(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label className='text-white font-medium mt-2 mb-3'>Fundo do link</label>
+                        <input type = "color" value = {backgroundColorInput} onChange={(e) => setBackgroundColorInput(e.target.value)}/>
+                    </div>
+                    <div className='flex items-center justify-start flex-col mb-7 p-1 border-gray-100/25 borded rounded-md'>
+                    <label className='text-white font-medium mt-2 mb-3'>Veja como está ficando:</label>
+                    <article className='w-11/12 max-w-lg flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3' style={{marginBottom:8, marginTop: 8, backgroundColor: backgroundColorInput}}>
+                        <p style ={{color:textColorInput}}>Canal do Youtube</p>
+                    </article>
+                    </div>
+                </section>
+            </form>
         </div>
     )
 }
