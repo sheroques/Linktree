b81c9b46f44ca878130698208b2196b10ff87cfa
sheroques <luizalexandre99@hotmail.com.br>
2025-02-06 11:19:08 -0300

    finalização do projeto

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 export function ErrorPage(){
     return(
         <div className="flex w-full min-h-screen justify-center items-center flex-col text-white">
+            <h1 className="font-bold text-6xl mb-2" >404</h1>
             <h1 className="font-bold text-4xl mb-4">Página não encontrada</h1>
             <p className="italic text-1xl mb-4">Voce caiu em uma pagina que não existe</p>
 
